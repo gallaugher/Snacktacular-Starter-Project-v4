39bcdbb7db8705312c95034af875105559e2edf2
John Gallaugher <john.gallaugher@bc.edu>
2020-06-10 11:43:45 -0400

    Ch. 8.3 Complete

---
+++ Snacktacular/SpotTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  SpotTableViewCell.swift
+//  Snacktacular
+//
+//  Created by John Gallaugher on 6/9/20.
+//  Copyright © 2020 John Gallaugher. All rights reserved.
+//
+
+import UIKit
+
+class SpotTableViewCell: UITableViewCell {
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    
+}
