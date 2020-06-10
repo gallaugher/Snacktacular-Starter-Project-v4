1176b6a8ff144dc124ecbce985270d6e2c0917ae
John Gallaugher <john.gallaugher@bc.edu>
2020-06-10 11:44:52 -0400

    Ch. 8.3 Complete - Basic UI Setup

---
+++ Snacktacular/SpotDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SpotDetailViewController.swift
+//  Snacktacular
+//
+//  Created by John Gallaugher on 6/9/20.
+//  Copyright © 2020 John Gallaugher. All rights reserved.
+//
+
+import UIKit
+
+class SpotDetailViewController: UIViewController {
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var addressTextField: UITextField!
+    @IBOutlet weak var ratingLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
+        let isPresentingInAddMode = presentingViewController is UINavigationController
+        if isPresentingInAddMode {
+            dismiss(animated: true, completion: nil)
+        } else {
+            navigationController?.popViewController(animated: true)
+        }
+    }
+    
+}
